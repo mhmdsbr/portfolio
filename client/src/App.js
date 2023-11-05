75a8ea45299a48ef4997e95fd22fdd82ec7fedb1
mhmd <saaber.mohamad@gmail.com>
2023-11-06 00:06:15 +0330

    Adding Wordpress Backend and docker

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,43 @@
-// App.js
 import React from 'react';
+import axios from "axios";
 
 import AppLayout from "./layout/AppLayout";
 import ScrollToTop from "./components/UI/button/ScrollToTop";
 
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-function App() {
-  return (
-      <div className="container-fluid app">
-          <AppLayout />
-          <ScrollToTop />
-      </div>
-  );
+        this.state = {
+            pages: []
+        };
+    }
+
+    getAllPages = async () => {
+        let res = await axios.get(
+            `http://portfolio.test/?rest_route=/wp/v2/pages`
+        );
+        let { data } = await res;
+
+        this.setState({ pages: data });
+    };
+
+    componentDidMount = async () => {
+        await this.getAllPages();
+    };
+
+    render() {
+        const { pages } = this.state;
+
+        return (
+          <div className="container-fluid app">
+              <AppLayout />
+              <ScrollToTop />
+              {pages.map((page, index) => {
+                  return <h1>{page.slug}</h1>;
+              })}
+          </div>
+
+        );
+    }
 }
-
-export default App;
-
