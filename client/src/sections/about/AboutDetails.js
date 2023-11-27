5a909080137ecfcdd08ffb5ed8e9180e105d8d45
mhmd <saaber.mohamad@gmail.com>
2023-11-28 01:54:55 +0330

    Responsive

--- client/src/sections/about/AboutDetails.js
+++ client/src/sections/about/AboutDetails.js
@@ -6,7 +6,7 @@ const AboutDetails = ({ detailsData }) => {
         <div className={`${classes['about__brands-grid']} separator-border mt-10`}>
             <div className="row">
                 {detailsData.map((item, index) => (
-                    <div className="col-6 col-md-3" key={index}>
+                    <div className="col-12 mb-6 mb-md-0 col-md-3" key={index}>
                         <div className={`${classes['about__brands-grid-item']} text-center`}>
                             <h4 className="mb-0 text-white-50">
                                 <span>{item.content}</span>+
