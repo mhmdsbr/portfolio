980d6e6867f33e40ce0c7cf29231b083ff70a133
mhmd <saaber.mohamad@gmail.com>
2023-11-27 16:12:55 +0330

    Follow us fix

--- client/src/components/UI/FollowUs.js
+++ client/src/components/UI/FollowUs.js
@@ -1,6 +1,7 @@
 import React, {useContext} from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {GeneralFieldsContext} from "../../store/GeneralFieldsContext";
+import classes from "./FollowUs.module.scss";
 
 const FollowUs = ({title}) => {
     const { linkedin, github, twitter, google } = useContext(GeneralFieldsContext);
@@ -14,18 +15,18 @@ const FollowUs = ({title}) => {
     const linksArray = Object.entries(socialMediaLinks).map(([icon, url]) => ({ icon, url }));
 
     return (
-        <React.Fragment>
-            <h2 className="mb-3 text-uppercase text-white">{title}</h2>
-            <ul className="social-icons d-flex gap-3 justify-content-center list-unstyled justify-content-md-start">
+        <div className={classes['social-icons']} >
+            <h2 className={`${classes['social-icons__title']} mb-3 text-uppercase text-white`}>{title}</h2>
+            <ul className={`${classes['social-icons__items']} social-icons d-flex gap-3 justify-content-center list-unstyled justify-content-md-start`}>
                 {linksArray?.map((link, index) => (
-                    <li className={`social-icons-${link.icon} text-secondary`} key={index}>
+                    <li className={`text-secondary`} key={index}>
                         <a href={link.url} target="_blank" rel="noreferrer">
-                            <FontAwesomeIcon icon={['fab', link.icon]} size="2x"/>
+                            <FontAwesomeIcon icon={['fab', link.icon]}/>
                         </a>
                     </li>
                 ))}
             </ul>
-        </React.Fragment>
+        </div>
     );
 };
 
